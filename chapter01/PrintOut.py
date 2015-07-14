c7e89b3530b3a14d394a706284406776333b122f
Solier <sbbmym@icloud.com>
2015-07-14 18:34:01 +0800

    modify PrintOut.py

--- chapter01/PrintOut.py
+++ chapter01/PrintOut.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 #-*-coding:utf-8-*-
 
-#递归函数
-#依次打印数字的每一位
+# #  FileName    : PrintOut.py
+# #  Author      : BaoBao SHI <sbbmym@icloud.com>
+# #  Created     : Tue July 14 18:28 2015 by BaoBao SHI
+# #  Copyright   : Feather (c) 2015
+# #  Description : 递归 打印数字的每一位
+# #  Time-stamp: <2015-07-14 18:30>
+
 
 def PrintOut(num):
 	if(num >= 10):
